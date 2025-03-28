bf62a2d84a32d08d68b44f9c376e3251f6dc97e8
Frank <frankli.consult@gmail.com>
2025-03-28 04:40:49 +0000

    temp: build

--- src/gen/external.data.v1.rs
+++ src/gen/external.data.v1.rs
@@ -286,17 +286,14 @@ pub struct WalletAccount {
     pub organization_id: ::prost::alloc::string::String,
     #[prost(string, tag="2")]
     pub wallet_id: ::prost::alloc::string::String,
-    // #[prost(enumeration="super::super::super::immutable::common::v1::Curve", tag="3")]
-    #[prost(string, tag="3")]
-    pub curve: String,
-    // #[prost(enumeration="super::super::super::immutable::common::v1::PathFormat", tag="4")]
-    #[prost(string, tag="4")]
-    pub path_format: String,
+    #[prost(enumeration="super::super::super::immutable::common::v1::Curve", tag="3")]
+    pub curve: i32,
+    #[prost(enumeration="super::super::super::immutable::common::v1::PathFormat", tag="4")]
+    pub path_format: i32,
     #[prost(string, tag="5")]
     pub path: ::prost::alloc::string::String,
-    // #[prost(enumeration="super::super::super::immutable::common::v1::AddressFormat", tag="6")]
-    #[prost(string, tag="6")]
-    pub address_format: String,
+    #[prost(enumeration="super::super::super::immutable::common::v1::AddressFormat", tag="6")]
+    pub address_format: i32,
     #[prost(string, tag="7")]
     pub address: ::prost::alloc::string::String,
     #[prost(message, optional, tag="8")]
